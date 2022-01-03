c20b53cc0fad3da3f857a27d907cb864dbcf6273
Thomas Schuff <tschuff5814@student.pps.net>
2022-01-03 13:43:08 -0800

    Added Movement Script For Players

---
+++ CLUE CREW 3D/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 playerVelocity;
+    public float playerSpeed = 6f;
+    Vector3 move = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horMove = Input.GetAxis("Horizontal");
+        float verMove = Input.GetAxis("Vertical");
+
+        Vector3 forward = gameObject.transform.forward;
+
+        if(forward.z != 0f)
+        {
+            move = new Vector3(horMove, 0.0f, verMove);
+        }
+        else if (forward.x != 0f)
+        {
+            move = new Vector3(verMove, 0.0f, horMove);
+        }
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+    }
+}
